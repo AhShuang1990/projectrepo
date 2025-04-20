b57cc2803fcfa4d1f7ae05a70a34946070a19f8e
AhShuang1990 <102665685+AhShuang1990@users.noreply.github.com>
2025-04-20 14:24:57 +0800

    Update feature.py
    collaborator update

--- feature.py
+++ feature.py
@@ -3,3 +3,9 @@ function sum(int num1, int num2){
     result = num1 + num2;
     return result;
 }
+
+function sum2 (int num3, int num4){
+    int result;
+    result = num3 + num4;
+    return result;
+}
